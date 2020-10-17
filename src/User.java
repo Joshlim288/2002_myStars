0fc7a920e59c75e68b3e43b259a137fdf10fa313
Jun Wei <tohjunwei.tj@gmail.com>
2020-10-17 17:50:55 +0800

    Implemented User

--- src/User.java
+++ src/User.java
@@ -1,4 +1,47 @@
+/**
+ * User represents a user in the STARS system.<br>
+ * It is inherited by Student and Admin.
+ *
+ * @author Jun Wei
+ * @version 1.0
+ * @since 2020-10-17
+ */
+
 public abstract class User {
+
+    /**
+     * This user's user ID.
+     */
     private String userID;
+
+    /**
+     * This user's password (hashed).
+     */
     private String hashedPassword;
+
+    /**
+     * Constructor for User, using userID and hashedPassword.
+     * @param userID This user's user ID.
+     * @param hashedPassword This user's password (hashed).
+     */
+    public User(String userID, String hashedPassword) {
+        this.userID = userID;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
 }
