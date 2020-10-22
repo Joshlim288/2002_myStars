d6a05bc51dedf50159d38dfcdc8839e8bdec3e65
Jun Wei <tohjunwei.tj@gmail.com>
2020-10-22 20:11:24 +0800

    Updated some attributes

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,7 @@
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Student represents a student enrolled in the university. It inherits from User.
@@ -9,7 +10,7 @@ import java.util.ArrayList;
  * @version 1.0
  * @since 2020-10-17
  */
-
+//TODO: add validations and error messages
 public class Student extends User {
 
     /**
@@ -29,14 +30,19 @@ public class Student extends User {
     private LocalDateTime[] accessTime;
 
     /**
-     * This student's AU balance.
+     * This student's max AUs possible for the semester.
      */
-    private int AUBalance;
+    private int maxAUs;
 
     /**
-     * This student's registered courses for the semester.
+     * This student's current AUs registered for the semester.
      */
-    private ArrayList<Course> coursesRegistered;
+    private int currentAUs;
+
+    /**
+     * This student's registered courses and the corresponding index for the semester.
+     */
+    private HashMap<Course, Index> coursesRegistered;
 
     /**
      * This student's major.
@@ -50,17 +56,17 @@ public class Student extends User {
      * @param hashedPassword This student's password (hashed).
      * @param studentName This student's name.
      * @param matricNum This student's matriculation number. It is unique.
-     * @param AUBalance This student's AU balance.
+     * @param maxAUs This student's AU balance.
      * @param major This student's major.
      */
-    public Student(String userID, String hashedPassword, String studentName, String matricNum, int AUBalance, String major) {
+    public Student(String userID, String hashedPassword, String studentName, String matricNum, int maxAUs, String major) {
         super(userID, hashedPassword);
         this.studentName = studentName;
         this.matricNum = matricNum;
-        this.AUBalance = AUBalance;
+        this.maxAUs = maxAUs;
         this.major = major;
         this.accessTime = new LocalDateTime[2];
-        this.coursesRegistered = new ArrayList<Course>();
+        this.coursesRegistered = new HashMap<Course, Index>();
     }
 
     public String getStudentName() {
@@ -79,12 +85,12 @@ public class Student extends User {
         this.matricNum = matricNum;
     }
 
-    public int getAUBalance() {
-        return AUBalance;
+    public int getMaxAUs() {
+        return maxAUs;
     }
 
-    public void setAUBalance(int AUBalance) {
-        this.AUBalance = AUBalance;
+    public void setMaxAUs(int maxAUs) {
+        this.maxAUs = maxAUs;
     }
 
     public String getMajor() {
@@ -99,16 +105,17 @@ public class Student extends User {
         return accessTime;
     }
 
-    public ArrayList<Course> getCoursesRegistered() {
+    public HashMap<Course, Index> getCoursesRegistered() {
         return coursesRegistered;
     }
 
     /**
      * Adds a course into this student's registered courses for the semester.
      * @param course The course to be added.
+     * @param index The index of the course to be added.
      */
-    public void addCourse(Course course) {
-        coursesRegistered.add(course);
+    public void addCourse(Course course, Index index) {
+        coursesRegistered.put(course, index);
     }
 
     /**
@@ -131,13 +138,13 @@ public class Student extends User {
      * @return <code>true</code> if the course is removed.
      */
     public boolean removeCourse(Course course) {
-        return coursesRegistered.remove(course);
+        return coursesRegistered.remove(course) != null;
     }
 
     /**
      * Compares this student with another student based on matriculation number.
      * @param o The other student to compare with this student.
-     * @return true if the other student has the same matriculation number as this student.
+     * @return <code>true</code> if the other student has the same matriculation number as this student.
      */
     @Override
     public boolean equals(Object o) {
